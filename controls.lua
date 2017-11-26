196949fb2b061804e5b193dbfbfba963038cb3d8
Josip Miskovic <josipmiskovic@gmail.com>
2017-11-26 13:49:14 +0100

    Support touch pressure as note velocity on devices if supported by device

--- controls.lua
+++ controls.lua
@@ -1,20 +1,23 @@
 local controls = {}
 
+local l = require('lume')
+
 controls.readTilt = function() return 0, 0, 0 end -- stub
 local tiltP = {0,0,0}
+
+local minPressure =  math.huge
+local maxPressure = -math.huge
 
 function controls.load()
   -- finding accelerometer
   local joysticks = love.joystick.getJoysticks()
   for i, joystick in ipairs(joysticks) do
-
     if joystick:getName() == 'Android Accelerometer' then
       controls.readTilt = function()
           return joystick:getAxis(1), joystick:getAxis(2), joystick:getAxis(3)
         end
       break
     end
-
   end
 end
 
@@ -26,7 +29,17 @@ function controls.process(s)
   for _,id in ipairs(touches) do
     local x, y = love.touch.getPosition(id)
     s.touches[id] = {x, y}
-    s.touches[id].pressure = love.touch.getPressure(id)
+    local pressure = love.touch.getPressure(id)
+    maxPressure = math.max(maxPressure, pressure)
+    minPressure = math.min(minPressure, pressure)
+    if minPressure < maxPressure then
+      -- eumulate note velocity with surface area of touch
+      s.touches[id].pressure = l.remap(pressure, minPressure, maxPressure, 0, 1)
+      s.pressureSupport = true
+    else  -- if same, let's not div by 0 (CPU might explode)
+      s.touches[id].pressure = pressure
+      s.pressureSupport = false
+    end
   end
   s.tilt = {controls.readTilt()}
 
@@ -40,8 +53,5 @@ function controls.process(s)
 
   return s
 end
-
--- legacy
-controls.tilt = {0,0,0}
 
 return controls
--- sampler.lua
+++ sampler.lua
@@ -3,82 +3,80 @@ sampler.__index = sampler
 
 local efx = require('efx')
 
+local samples = {}
 
 function sampler.new(settings)
   local self = setmetatable({}, sampler)
 
   self.synths = {} -- collection of sources in a map
-  self.transpose    = settings.transpose or 0
   local synthCount  = settings.synthCount or 6
   local path        = settings.path
-  local looped      = settings.looped or false
 
-  self.envelope = settings.envelope or {
-    attack  = 0,
-    decay   = 0,
-    sustain = 1,
-    release = 0.35,
-  }
-  self.slopes = self:getSlopes(self.envelope)
+  self.looped       = settings.looped or false
   self.masterVolume = 1
 
-  local decoder = love.sound.newDecoder(path)
-  local soundData = love.sound.newSoundData(decoder)
+  self.envelope = settings.envelope or { attack  = 0,
+                                         decay   = 0,
+                                         sustain = 1,
+                                         release = 0.35 }
 
+  -- prepare samples that will be used by synths
+  for i,sample in ipairs(settings) do
+    local decoder = love.sound.newDecoder(sample.path)
+    sample.soundData = love.sound.newSoundData(decoder)
+    sample.transpose = sample.transpose or 0
+    sample.velocity  = sample.velocity or 0.8
+    table.insert(samples, sample)
+  end
+
+  -- initialize synths
   for i=1, synthCount do
     self.synths[i] = {
-      source = love.audio.newSource(soundData),
+      source = nil,
       volume = 0,
       active = false,
       duration = math.huge,
       enveloped = 0,
     }
-    self.synths[i].source:setLooping(looped)
-    self.synths[i].source:stop()
-    efx.applyFilter(self.synths[i].source)
   end
   return self
 end
 
 function sampler:update(dt, touches)
-  -- hunt for new touches and play them
-  for id, touch in pairs(touches) do
-    if touch.noteRetrigger then
-      local synth = self:assignSynth(id)
-      synth.touchId = id
-      synth.duration = 0
-      synth.enveloped = 0
-      synth.active = true
-      synth.source:stop()
-      synth.source:play()
-      synth.source:setPosition(touch.qr[1]/4, touch.qr[2]/4, 0.5)
-    end
-  end
   -- update sources for existing touches
   for i, synth in ipairs(self.synths) do
     local touch = touches[synth.touchId]
+
     if touch and touch.note then           -- update existing notes
-      local pitch = self:noteToPitch(touch.note)
+      local pitch = self:noteToPitch(touch.note, synth.transpose)
       synth.source:setPitch(pitch)
     else
       synth.active = false                 -- not pressed, let envelope release
     end
     synth.enveloped = self:applyEnvelope(dt, synth.enveloped, synth.active, synth.duration)
     local volume = synth.enveloped * self.masterVolume
-    synth.source:setVolume(volume)
+    if synth.source then
+      synth.source:setVolume(volume)
+    end
     if touches[synth.touchId] then
       touches[synth.touchId].volume = volume
     end
     synth.duration = synth.duration + dt
   end
+  -- hunt for new touches and play them
+  for id, touch in pairs(touches) do
+    if touch.noteRetrigger then
+      self:assignSynth(id, touch)
+    end
+  end
 end
 
-function sampler:noteToPitch(note)
+function sampler:noteToPitch(note, transpose)
   -- equal temperament
-  return math.pow(math.pow(2, 1/12), note + self.transpose)
+  return math.pow(math.pow(2, 1/12), note + transpose)
 end
 
-function sampler:assignSynth(touchId)
+function sampler:assignSynth(touchId, touch)
   -- find synth with longest duration
   maxDuration = -100
   selected = nil
@@ -90,32 +88,50 @@ function sampler:assignSynth(touchId)
   end
   -- move source to correct key
   local synth = self.synths[selected]
+  -- init and play
+  if synth.source then
+    synth.source:stop()
+  end
+  local sample = self:assignSample(touch.note, touch.pressure or 1)
+  synth.source = love.audio.newSource(sample.soundData)
   synth.touchId = touchId
+  synth.duration = 0
+  synth.enveloped = 0
+  synth.active = true
+  synth.transpose = sample.transpose
+  efx.applyFilter(synth.source)
+  synth.source:setPosition(touch.qr[1]/4, touch.qr[2]/4, 0.5)
+  synth.source:setLooping(self.looped)
+  synth.source:play()
   return synth
 end
 
-function sampler:getSlopes(envelope)
-  return {
-    attack  = 1 / envelope.attack,
-    decay   = -(1 - envelope.sustain) / envelope.decay,
-    sustain = 0,
-    release = - envelope.sustain / envelope.release,
-  }
+function sampler:assignSample(note, velocity)
+  local bestFitness = math.huge
+  local selected = nil
+  for i, sample in ipairs(samples) do
+    local fitness = math.abs(-sample.transpose - note) + math.abs(sample.velocity - velocity)
+    if fitness < bestFitness then
+      selected = i
+      bestFitness = fitness
+    end
+  end
+  return samples[selected]
 end
 
-function sampler:applyEnvelope(dt, enveloped, active, duration)
+function sampler:applyEnvelope(dt, vol, active, duration)
   if active then
-    if self.envelope.attack == 0 and duration < 0.01 then                                 -- flat
+    if self.envelope.attack == 0 and duration < 0.01 then             -- flat
       return self.envelope.sustain
     elseif duration < self.envelope.attack then                       -- attack
-      return enveloped + self.slopes.attack * dt
+      return vol + 1 / self.envelope.attack * dt
     elseif duration < self.envelope.attack + self.envelope.decay then -- decay
-      return enveloped + self.slopes.decay * dt
+      return vol - (1 - self.envelope.sustain) / self.envelope.decay * dt
     else                                                              -- sustain
-      return enveloped
+      return vol
     end
   else                                                                -- release
-    return math.max(0, enveloped + self.slopes.release * dt)
+    return math.max(0, vol - self.envelope.sustain / self.envelope.release * dt)
   end
 end
 
