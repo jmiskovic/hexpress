bfa2f7791e0b52f4e4c654924add15d1a8f1607f
Josip Miskovic <josipmiskovic@gmail.com>
2018-02-26 08:01:43 +0100

    New stringed bass instrument

--- fretboard.lua
+++ fretboard.lua
@@ -11,7 +11,8 @@ fretboard.colorScheme = {
   light   = {l.rgba(0xffffffc0)},
 }
 
-fretboard.neckWidth = 1.85  -- 1.0 is screen height
+fretboard.neckHeight = 1.85  -- with 1.0 being half-screen height
+fretboard.neckWidth = 3.2
 fretboard.fretWidth = 0.4
 
 function fretboard.load()
@@ -21,9 +22,9 @@ function fretboard.new(displayNoteNames, tuning)
   -- tuning names are from highest to lowest pitch, but note index tables
   -- are from lowest to highest (a bit confusing)
   local tuning_presets = {
-    ['EBGDAE'] = {-20, -15, -10, -5, -1, 4, }, -- standard guitar
-    ['EBGDAD'] = {-22, -15, -10, -5, -1, 4, }, -- dropped D guitar
-    ['DBGDGD'] = {-20, -15, -10, -5, -1, 4, }, -- open G guitar
+    ['EBGDAE'] = {-20, -15, -10,  -5, -1, 4}, -- standard guitar
+    ['EBGDAD'] = {-22, -15, -10,  -5, -1, 4}, -- dropped D guitar
+    ['DBGDGD'] = {-20, -15, -10,  -5, -1, 4}, -- open G guitar
     ['GDAE']   = {-32, -27, -22, -17},         -- bass
     ['EADG']   = {-17, -10,  -3,   4},         -- violin
   }
@@ -59,9 +60,9 @@ function fretboard:interpret(s)
     love.graphics.pop()
     -- check if string is pressed, report string, fret and note
     local stringI, fretI
-    stringI = l.remap(y, self.neckWidth / 2, -self.neckWidth / 2, 1, #self.strings)
+    stringI = l.remap(y, self.neckHeight / 2, -self.neckHeight / 2, 1, #self.strings)
     stringI = math.floor(stringI + 0.5)
-    fretI = math.floor(l.remap(x, -2, 2, 0, 4 / self.fretWidth))
+    fretI = math.ceil(l.remap(x, -self.neckWidth/2, self.neckWidth/2, 0, self.neckWidth / self.fretWidth))
 
     if stringI >= 1 and stringI <= #self.strings then
       if self.activeNotes[id] and self.activeNotes[id].string == stringI then
@@ -84,7 +85,7 @@ function fretboard:interpret(s)
 end
 
 function fretboard:toX(fret)
-  return l.remap(fret, 0, 4 / self.fretWidth, -2, 2)
+  return l.remap(fret, 0, self.neckWidth / self.fretWidth, -self.neckWidth/2, self.neckWidth/2)
 end
 
 function fretboard:toY(string)
@@ -92,22 +93,22 @@ function fretboard:toY(string)
   if stringCount == 1 then
     return 0
   else
-    return l.remap(string, 1, stringCount, self.neckWidth / 2, -self.neckWidth / 2)
+    return l.remap(string, 1, stringCount, self.neckHeight / 2, -self.neckHeight / 2)
   end
 end
 
 function fretboard:draw(s)
   love.graphics.setColor(self.colorScheme.neck)
-  love.graphics.rectangle('fill', -15, -self.neckWidth / 2 * 1.05, 30, self.neckWidth * 1.05)
+  love.graphics.rectangle('fill', -15, -self.neckHeight / 2 * 1.05, 30, self.neckHeight * 1.05)
 
   -- draw frets
   love.graphics.setLineWidth(0.125 * self.fretWidth)
-  for toX = -2, 2, self.fretWidth do
+  for toX = -self.neckWidth/2, self.neckWidth/2, self.fretWidth do
     love.graphics.setColor(self.colorScheme.fret)
-    love.graphics.line(toX, -self.neckWidth / 2 * 1.05, toX, self.neckWidth / 2 * 1.05)
+    love.graphics.line(toX, -self.neckHeight / 2 * 1.05, toX, self.neckHeight / 2 * 1.05)
     local dx = 0.01
     love.graphics.setColor(self.colorScheme.light)
-    love.graphics.line(toX + dx, -self.neckWidth / 2 * 1.05, toX + dx, self.neckWidth / 2 * 1.05)
+    love.graphics.line(toX + dx, -self.neckHeight / 2 * 1.05, toX + dx, self.neckHeight / 2 * 1.05)
   end
   -- draw strings
   for i = 1, #self.strings do
