766ca75753e50f402abb210715d4c09b5ba1dade
Josip Miskovic <josipmiskovic@gmail.com>
2019-10-03 12:24:46 +0200

    New electric piano instrument

--- patches/wurl/wurl.lua
+++ patches/wurl/wurl.lua
@@ -10,7 +10,7 @@ local hexgrid = require('hexgrid')
 local controls = require('controls')
 
 local colorScheme = {
-  background = {l.rgba(0x0a0a0cff)},
+  background = {l.rgba(0xffffffff)},
   highlight  = {l.hsl(0.88, 0.56, 0.46)},
   surface    = {l.hsl(0.66, 0.25, 0.26)},
   surfaceC   = {l.hsl(0.66, 0.20, 0.23)},
@@ -35,31 +35,33 @@ local colorScheme = {
   },
 }
 
-local noteTracker = {}
 local keyCenter = 0
+local noteTracker = {}       -- seconds since last note trigger, across octaves
+local noteOctaveTracker = {10,10,10,10,10,10,10,10,10,10,10,10} -- seconds since last note trigger, flattened to single octave
+noteOctaveTracker[0] = 10
+
 
 function patch.load()
   efx.addEffect(efx.tremolo)
   efx.setDryVolume(0.4)
-  efx.reverb.volume = 1
-  efx.reverb.decaytime = 2
-  efx.tremolo.volume = 1
-  efx.tremolo.frequency = 4
+  efx.reverb.volume = 0.2
+  efx.reverb.decaytime = 4
+  efx.tremolo.volume = 0.25
+  efx.tremolo.frequency = 3
 
   patch.keyboard = hexpad.new(true)
 
   patch.synth = sampler.new({
-    {path='patches/chromakey/synthpad.ogg',  note= notes.toIndex['C3']},
+    {path='patches/wurl/wurl_a0.ogg', note = notes.toIndex['A0']},
+    {path='patches/wurl/wurl_a1.ogg', note = notes.toIndex['A1']},
+    {path='patches/wurl/wurl_a2.ogg', note = notes.toIndex['A2']},
+    {path='patches/wurl/wurl_a3.ogg', note = notes.toIndex['A3']},
     envelope = { attack = 0, decay = 0, sustain = 1, release = 0.15 },
-    synthCount = 10,
     transpose = -24,
+    synthCount = 6,
     })
-  patch.sustain = sampler.new({
-    {path='patches/chromakey/sustain.ogg',  note= notes.toIndex['C5']},
-    envelope = { attack = 6, decay = 0, sustain = 1, release = 0.15 },
-    looped = true,
-    synthCount = 10,
-    })
+
+  patch.synth.masterVolume = 1
 
   patch.keyboard.colorScheme.background = colorScheme.background
   patch.keyboard.colorScheme.highlight  = colorScheme.highlight
@@ -75,17 +77,40 @@ function patch.drawCell(self, q, r, s, touch)
   local note = self:toNoteIndex(q, r)
   love.graphics.scale(.90)
 
+  local noteTime = noteOctaveTracker[note % 12] -- time since last triggered
+  local nh, ns, nl = unpack(colorScheme.noteColors[math.floor(note-keyCenter+.5) % 12 + 1])
+  local noteDecay = 10
   local ch, cs, cl
 
   if s.tilt[1] > .9 and note ~= keyCenter then
-    ch, cs, cl = .0, .1, .1
+    ch, cs, cl = .0, .1, 0.9
   else
-    ch, cs, cl = unpack(colorScheme.noteColors[math.floor(note-keyCenter+.5) % 12 + 1])
-    cl = cl * (1 - math.exp(-(noteTracker[note % 12] or 0)/2))
+    ch, cs = nh, ns
+    cl = l.remap(noteTime, noteDecay * 0.9, noteDecay, nl, 1, 'clamp')
   end
 
   love.graphics.setColor(l.hsl(ch, cs, cl))
-  love.graphics.circle('fill', 0, 0, 0.8)
+  love.graphics.push()
+
+  local freq = 40
+  local ampl = l.remap(noteTime, 0, 1, math.pi/12, 0, 'clamp')
+  --love.graphics.scale(0.1)
+  local scl = 0.3
+  if (noteTracker[note] or math.huge) / noteDecay < 1 then
+    scl = 0.9
+  elseif ((noteTracker[note + 12] or math.huge) / noteDecay < 1
+       or (noteTracker[note - 12] or math.huge) / noteDecay < 1) then
+    scl = 0.7
+  elseif ((noteTracker[note + 24] or math.huge) / noteDecay < 1
+       or (noteTracker[note - 24] or math.huge) / noteDecay < 1) then
+    scl = 0.5
+  end
+  scl = scl + ampl / 8 * math.sin(freq * noteTime)
+  scl = l.remap(noteTime, 0, 0.1, 0.6, scl, 'clamp')
+  --love.graphics.rotate(ampl * math.sin(freq * noteTime))
+  love.graphics.scale(scl)
+  love.graphics.circle('fill', 0, 0, 0.93, 6) --love.mouse.getY() / love.graphics.getHeight() * 2
+  love.graphics.pop()
   if self.displayNoteNames then
     -- note name text
     love.graphics.scale(0.01)
@@ -93,35 +118,39 @@ function patch.drawCell(self, q, r, s, touch)
     love.graphics.setFont(self.font)
     local h = self.font:getHeight()
     local w = self.font:getWidth(text)
-    cl = (cl + 0.5) % 1
-    love.graphics.setColor(l.hsl(ch, cs, cl))
+    local tl = (cl + 0.5) % 1
+    love.graphics.setColor(l.hsl(nh, ns, tl))
     love.graphics.print(text, -w / 2, -h / 2)
   end
 end
 
 function patch.process(s)
+  love.graphics.setBackgroundColor(colorScheme.background)
+
   patch.keyboard:interpret(s)
   for _,touch in pairs(s.touches) do
     touch.pressure = l.remap(s.tilt[2], 0.2, 0.7, 0.1, 1, 'clamp')
     if touch.noteRetrigger then
-      noteTracker[touch.note  % 12] = (noteTracker[touch.note % 12] or 0) + 20000
+      noteOctaveTracker[touch.note  % 12] = 0
+      noteTracker[touch.note] = 0
     end
     if s.tilt[1] > .9 then
       keyCenter = touch.note
     end
   end
-  patch.synth.masterVolume = l.remap(s.tilt[2], 0.2, 0.7, 0.2, 1, 'clamp')
-  patch.sustain.masterVolume = l.remap(s.tilt[2], 0.2, 0.7, 0.2, 1, 'clamp')
 
-  efx.tremolo.frequency = l.remap(s.tilt.lp[1], 0.05, 0.3, 0, 8, 'clamp')
-  efx.tremolo.volume = l.remap(s.tilt.lp[1], 0, 1, 0, 0.9, 'clamp')
+  efx.tremolo.frequency = l.remap(s.tilt.lp[1], -0.2, 0.2, 0, 5, 'clamp')
+  patch.synth.envelope.attack = l.remap(s.tilt.lp[2], -.1, -0.3, 0, 0.8, 'clamp')
 
   patch.synth:processTouches(s.dt, s.touches)
-  patch.sustain:processTouches(s.dt, s.touches)
 
   for note,decay in pairs(noteTracker) do
-    noteTracker[note] = decay * (1 - s.dt * 1)
+    noteTracker[note] = decay + s.dt
   end
+  for note,decay in pairs(noteOctaveTracker) do
+    noteOctaveTracker[note] = decay + s.dt
+  end
+
 end
 
 function patch.draw(s)
@@ -157,7 +186,7 @@ function patch.icon(time)
       love.graphics.scale(0.15)
       local x, y = hexgrid.hexToPixel(q, r)
       love.graphics.translate(x,y)
-      love.graphics.circle('fill', x, y, 1.2 * size)
+      love.graphics.circle('fill', x, y, 1.8 * size, 6)
     love.graphics.pop()
     i = i + 1
   end
